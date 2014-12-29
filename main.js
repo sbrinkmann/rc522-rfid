8dc33a2864734429ece43fc59ed6b3da1f7fa3d2
sbrinkmann <sascha.brinkmann@gmail.com>
2014-12-29 23:53:56 +0100

    Added child process in case that the main process ends

--- main.js
+++ main.js
@@ -9,6 +9,8 @@ module.exports = exports = function(givenCallback){
 	registeredCallback = givenCallback;
 };
 
+var mainProcessShutdown = false;
+
 var initChildProcess = function()
 {
 	var child = spawn("node", [__dirname + "/" + "rc522_output.js"]);
@@ -22,8 +24,31 @@ var initChildProcess = function()
 	});
 
 	child.on('close', function(code) {
-		initChildProcess();
+		if(!mainProcessShutdown)
+		{
+			initChildProcess();
+		}
 	});
 };
+
+// And the exit event shuts down the child.
+process.once("exit", function () {
+	mainProcessShutdown = true;
+	child.shutdown();
+});
+
+// This is a somewhat ugly approach, but it has the advantage of working
+// in conjunction with most of what third parties might choose to do with
+// uncaughtException listeners, while preserving whatever the exception is.
+process.once("uncaughtException", function (error) {
+	// If this was the last of the listeners, then shut down the child and rethrow.
+	// Our assumption here is that any other code listening for an uncaught
+	// exception is going to do the sensible thing and call process.exit().
+	if (process.listeners("uncaughtException").length === 0) {
+		mainProcessShutdown = true;
+		child.shutdown();
+		throw error;
+	}
+});
 
 initChildProcess();
